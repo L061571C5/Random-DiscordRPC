a9dea64eaea5c5e40ccd778f300543f01dcf711c
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2021-03-10 20:09:55 -0600

    fixes

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-const clientId = '806211605326594079';
 const DiscordRPC = require('discord-rpc');
-DiscordRPC.register(clientId);
-const stuff = require('./assets/assets.json')
 const rpc = new DiscordRPC.Client({ transport: 'ipc' });
 const startTimestamp = new Date();
-
+const random = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '_', '+', '{', '}', '[', ']'];
 async function setActivity() {
     rpc.setActivity({
-        details: randomProperty(stuff.details),
-        state: randomProperty(stuff.state),
-        startTimestamp,
-        largeImageKey: randomProperty(stuff.largeImage),
-        largeImageText: randomProperty(stuff.largeImageText),
+        details: randomProperty(),
+        state: randomProperty(),
+        startTimestamp: startTimestamp,
+        largeImageKey: 'dice',
+        largeImageText: randomProperty(),
         instance: false,
     });
 }
 
-rpc.on('ready', () => {
+rpc.on('connected', () => {
+    console.log(`Done!`)
     setActivity();
-
-    // activity can only be set every 15 seconds
     setInterval(() => {
         setActivity();
-    }, 15e3);
+    }, 150);
 });
-function randomProperty(obj) {
-    var keys = Object.keys(obj);
-    return obj[keys[keys.length * Math.random() << 0]];
+
+function randomProperty() {
+    return `${random[Math.floor(Math.random() * random.length)]}${random[Math.floor(Math.random() * random.length)]}${random[Math.floor(Math.random() * random.length)]}${random[Math.floor(Math.random() * random.length)]}${random[Math.floor(Math.random() * random.length)]}${random[Math.floor(Math.random() * random.length)]}${random[Math.floor(Math.random() * random.length)]}${random[Math.floor(Math.random() * random.length)]}${random[Math.floor(Math.random() * random.length)]}${random[Math.floor(Math.random() * random.length)]}${random[Math.floor(Math.random() * random.length)]}${random[Math.floor(Math.random() * random.length)]}${random[Math.floor(Math.random() * random.length)]}${random[Math.floor(Math.random() * random.length)]}${random[Math.floor(Math.random() * random.length)]}${random[Math.floor(Math.random() * random.length)]}`
 };
-rpc.login({ clientId }).catch(console.error);
+
+rpc.login({ clientId: '806211605326594079' }).catch(console.error).then(() => {
+    setActivity();
+    setInterval(() => {
+        setActivity();
+    }, 15)
+})
