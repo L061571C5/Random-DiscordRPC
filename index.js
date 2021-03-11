1f4c38c82a2a8ba6bf54f653f9ee18ec92bd41bf
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2021-03-10 20:33:51 -0600

    stuff

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const DiscordRPC = require('discord-rpc');
 const rpc = new DiscordRPC.Client({ transport: 'ipc' });
-const startTimestamp = new Date();
-const random = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '_', '+', '{', '}', '[', ']'];
+const random = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '_', '+', '{', '}', '[', ']', 'ᔑ', 'ʖ', 'ᓵ', '↸', 'ᒷ', '⎓', '⊣', '⍑', '╎', '⋮', 'ꖌ', 'ꖎ', 'ᒲ', 'リ', '𝙹', '!', '¡', 'ᑑ', '∷', 'ᓭ', 'ℸ', ' ̣ ', '⚍', '⍊', '∴', '/', '|', '|', '⨅'];
 async function setActivity() {
     rpc.setActivity({
         details: randomProperty(),
         state: randomProperty(),
-        startTimestamp: startTimestamp,
+        startTimestamp: new Date(),
         largeImageKey: 'dice',
         largeImageText: randomProperty(),
         instance: false,
@@ -18,7 +17,7 @@ rpc.on('connected', () => {
     setActivity();
     setInterval(() => {
         setActivity();
-    }, 150);
+    }, 100);
 });
 
 function randomProperty() {
@@ -29,5 +28,5 @@ rpc.login({ clientId: '806211605326594079' }).catch(console.error).then(() => {
     setActivity();
     setInterval(() => {
         setActivity();
-    }, 15)
+    }, 100)
 })
